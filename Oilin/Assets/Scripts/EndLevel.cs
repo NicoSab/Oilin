a9a197c61a651cfe64253916562caacd1b5b2579
Yayap <nicolas.sabella@epita.fr>
2014-06-03 13:32:43 +0200

    Add script enemy

---
+++ Oilin/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour {
+
+	private float timeToEnd = 3f;
+	private GameObject player;                      // Reference to the player.
+	private Animator playerAnim;                    // Reference to the players animator component.
+	private CameraMovement camMovement;             // Reference to the camera movement script.
+	private ScreenFader sf;	 						// Reference to the screen fader;
+	private float timer;
+	private float end1 = 1f;
+	private float end2 = 2f;
+	private bool col = false;
+	private PlayerKey playerKey;
+
+	void Awake ()
+	{
+		// Setting up references.
+		player = GameObject.Find("N40");
+		playerAnim = player.GetComponent<Animator>();
+		playerKey = player.GetComponent<PlayerKey>();
+		camMovement = Camera.main.gameObject.GetComponent<CameraMovement>();
+		sf = GameObject.Find("ScreenFader").GetComponent<ScreenFader>();
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject == player && playerKey.playerHasKey)
+		{
+			col = true;
+		}
+	}
+
+
+	public void EndOfLevel()
+	{
+		timer += Time.deltaTime;
+
+		if(timer >= end1)
+		{
+			playerAnim.SetFloat("Speed", 0f);
+			camMovement.enabled = false;
+			player.transform.parent = transform;
+		}
+
+		if(timer >= end2)
+			sf.FadeScreen();
+	}
+
+	void Update()
+	{
+		if (col)
+			EndOfLevel();
+	}
+}
---
+++ Oilin/Assets/Scripts/LevelDynamics/DoorAnimation.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System.Collections;
+
+public class DoorAnimation : MonoBehaviour
+{
+	public bool requireKey;							// Whether or not a key is required.
+	public AudioClip doorSwishClip;					// Clip to play when the doors open or close.
+	public AudioClip accessDeniedClip;				// Clip to play when the player doesn't have the key for the door.
+	
+	
+	private Animator anim;							// Reference to the animator component.
+	private GameObject player;						// Reference to the player GameObject.
+	private PlayerKey playerInventory;	// Reference to the PlayerInventory script.
+	private int count;								// The number of colliders present that should open the doors.
+	
+	
+	void Awake ()
+	{
+		// Setting up the references.
+		anim = GetComponent<Animator>();
+		player = GameObject.Find("N40");
+		playerInventory = player.GetComponent<PlayerKey>();
+	}
+	
+	
+	void OnTriggerEnter (Collider other)
+	{
+		// If the triggering gameobject is the player...
+		if(other.gameObject == player)
+		{
+			// ... if this door requires a key...
+			if(requireKey)
+			{
+				// ... if the player has the key...
+				if(playerInventory.playerHasKey)
+					// ... increase the count of triggering objects.
+					count++;
+				else
+				{
+					// If the player doesn't have the key play the access denied audio clip.
+					audio.clip = accessDeniedClip;
+					audio.Play();
+				}
+			}
+			else
+				// If the door doesn't require a key, increase the count of triggering objects.
+				count++;
+		}
+		// If the triggering gameobject is an enemy...
+		else if(other.gameObject.tag == "enemy")
+		{
+			// ... if the triggering collider is a capsule collider...
+			if(other is CapsuleCollider)
+				// ... increase the count of triggering objects.
+				count++;
+		}
+	}
+	
+	
+	void OnTriggerExit (Collider other)
+	{
+		// If the leaving gameobject is the player or an enemy and the collider is a capsule collider...
+		if(other.gameObject == player || (other.gameObject.tag == "enemy" && other is CapsuleCollider))
+			// decrease the count of triggering objects.
+			count = Mathf.Max(0, count-1);
+	}
+	
+	
+	void Update ()
+	{
+		// Set the open parameter.
+		anim.SetBool("Open", count > 0);
+		
+		// If the door is opening or closing...
+		if(anim.IsInTransition(0) && !audio.isPlaying)
+		{
+			// ... play the door swish audio clip.
+			audio.clip = doorSwishClip;
+			audio.Play();
+		}
+	}
+}
