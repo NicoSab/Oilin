80e5a0c253101d83d0507bcadce6a4750e4b3a91
Julien <jcze91@gmail.com>
2014-06-08 02:20:23 +0200

    level selector + git rm .meta

---
+++ Oilin/Assets/LeftArrowGesture.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftArrowGesture : MonoBehaviour {
+
+	Material normal;
+	Material hover;
+	public bool showed;
+	float timer = 0f;
+	float maxx = 0.5f;
+	float maxy = 0.4f;
+	float maxz = 0.4f;
+	GameObject play;
+	PlayGesture playgesture;
+
+	// Use this for initialization
+	void Start () {
+		play = GameObject.Find ("play");
+		playgesture = play.GetComponent<PlayGesture>();
+		normal = Resources.Load ("leftarrow", typeof(Material)) as Material;
+		hover = Resources.Load ("leftarrowhover", typeof(Material)) as Material;
+	}
+	void OnMouseDown()
+	{
+		playgesture.RewardIndex ();
+	}
+	void OnMouseOver()
+	{
+		renderer.material = hover;
+	}
+	void OnMouseExit()
+	{
+		renderer.material = normal;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (showed && timer < 3f)
+		{
+			Vector3 v = new Vector3();
+			v.x = maxx * timer / 3f;
+			v.y = maxy * timer / 3f;
+			v.z = maxz * timer / 3f;
+			transform.localScale = v;
+			timer += 0.5f;
+		}
+	}
+}
--- Oilin/Assets/PlayGesture.cs
+++ Oilin/Assets/PlayGesture.cs
@@ -5,26 +5,63 @@ public class PlayGesture : MonoBehaviour {
 
 	Material normal;
 	Material hover;
+	public bool chooseLevel = false;
+	int indexLevel = 1;
+	GameObject ra, la;
+	RightArrowGesture rightArrow;
+	LeftArrowGesture leftArrow;
 	// Use this for initialization
 	void Start () {
 		normal = Resources.Load ("play", typeof(Material)) as Material;
 		hover = Resources.Load ("playhover", typeof(Material)) as Material;
+		ra = GameObject.Find ("rightarrow");
+		la = GameObject.Find ("leftarrow");
+		rightArrow = ra.GetComponent<RightArrowGesture>();
+		leftArrow = la.GetComponent<LeftArrowGesture>();
+
+	}
+	public void RewardIndex()
+	{
+		if (indexLevel > 1)
+			indexLevel--;
+		renderer.material = Resources.Load ("level" + indexLevel.ToString(), typeof(Material)) as Material;
+	}
+	public void ForwardIndex()
+	{
+		if (indexLevel < 5)
+			indexLevel++;
+		renderer.material = Resources.Load ("level" + indexLevel.ToString(), typeof(Material)) as Material;
+
 	}
 	void OnMouseDown()
 	{
-		Application.LoadLevel ("level1");
+		if (!chooseLevel)
+		{
+			rightArrow.showed = true;
+			leftArrow.showed = true;
+
+			chooseLevel = true;
+		} else {
+			Application.LoadLevel ("level" + indexLevel.ToString());
+		}
 	}
 	void OnMouseOver()
 	{
-		renderer.material = hover;
+		if (chooseLevel) {
+			renderer.material = Resources.Load ("level" + indexLevel.ToString() + "hover", typeof(Material)) as Material;
+		}
+		else
+			renderer.material = hover;
 	}
 	void OnMouseExit()
 	{
-		renderer.material = normal;
+		if (chooseLevel)
+			renderer.material = Resources.Load ("level" + indexLevel.ToString(), typeof(Material)) as Material;
+		else
+			renderer.material = normal;
 	}
 
 	// Update is called once per frame
 	void Update () {
-
 	}
 }
